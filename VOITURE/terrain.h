735dc34508d7e2523b1aebbd8e0c3e14c70bfafb
SamuelDiai <38350776+SamuelDiai@users.noreply.github.com>
2018-05-11 15:49:58 +0200

    Update terrain.h

--- VOITURE/terrain.h
+++ VOITURE/terrain.h
@@ -1,4 +1,25 @@
 #ifndef TERRAIN_H
 #define TERRAIN_H
+#include <iostream>
+#include <Imagine/Graphics.h>
+using namespace Imagine;
+#include <queue>
+#define USE_MATH_DEFINES
+#include "math.h"
 
+const int hauteur=400;
+const int largeur=400;
+class terrain
+{
+    std::vector<int> image;
+
+public:
+    terrain();
+    int f(int i);
+    void afficher();
+    void pop_front();
+    void defiler(int p);
+};
+
+int fonction(int x);
 #endif // TERRAIN_H
