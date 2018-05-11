9e86e02018cc4dc56d7a37251ed536d838dcb28d
SamuelDiai <38350776+SamuelDiai@users.noreply.github.com>
2018-05-11 15:48:53 +0200

    Update main.cpp

--- VOITURE/main.cpp
+++ VOITURE/main.cpp
@@ -1,4 +1,19 @@
+#include "terrain.h"
+
 int main()
 {
+    srand((unsigned int)time(0));
+    terrain t;
+    openWindow(largeur,hauteur);
+    int x=0;
+    while(true)
+    {
+        int p=fonction(x);
+        x+=1;
+        t.defiler(p);
+        std::cout <<t.f(399)<<"  "<< fonction(x)<<std::endl;
+        t.afficher();
+        milliSleep(10);
+    }
     return 0;
 }
