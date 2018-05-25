97c45491e3e870d75c41d74c2aaad194bcb65360
SamuelDiai <38350776+SamuelDiai@users.noreply.github.com>
2018-05-25 20:32:43 +0200

    Update terrain.cpp

--- VOITURE/terrain.cpp
+++ VOITURE/terrain.cpp
@@ -1,29 +1,84 @@
-#ifndef TERRAIN_H
-#define TERRAIN_H
-#include <iostream>
-#include <Imagine/Graphics.h>
-using namespace Imagine;
-#include "voiture.h"
+#include "terrain.h"
 
-#define USE_MATH_DEFINES
-#include "math.h"
-
-
-float distance(FVector<float,2> a ,FVector<float,2> b);
-class terrain
+float distance(FVector<float,2> a ,FVector<float,2> b)
 {
-    std::vector<int> image;
+    return sqrt((a.x()-b.x())*(a.x()-b.x())+(a.y()-b.y())*(a.y()-b.y()));
+}
 
-public:
-    terrain();
-    int f(int i);
-    void afficher(int posb);
-    void pop_front();
-    void defiler(int p);
-    bool contact(Balle b);
-    bool contact_air(Balle b);
+terrain::terrain()
+{
+    for (int j=0;j<6*largeur;j++)
+    {
+        int p(fonction(j));
+        image.push_back(p);
+    }
+}
 
-};
+void terrain::afficher(int posb)
+{
 
-int fonction(int x);
-#endif // TERRAIN_H
+    clearWindow();
+    for (int i=posb-largeur/3;i<posb-largeur/3+image.size();i++)
+    {
+
+        fillRect(-posb+largeur/3+i,image[i],1,1,RED);
+    }
+
+}
+
+void terrain::pop_front()
+{
+    image.erase(image.begin());
+}
+
+void terrain::defiler(int p)
+{
+
+    if (image.size()>largeur)
+    {
+        image.push_back(p);
+        pop_front();
+    }
+    else
+    {
+        image.push_back(p);
+    }
+}
+
+bool terrain::contact(Balle b)
+{
+    int abs = b.getpos().x();
+    FVector<float,2> poscourbe(abs,fonction(abs));
+    FVector<float,2> posballe(b.getpos());
+    return distance(poscourbe,posballe)< b.r;
+}
+
+bool terrain::contact_air(Balle b)
+{
+    if (!contact(b))
+        return false;
+    else
+    {
+        float y=b.getpos().x()-dt*b.getvitx();
+        float diff=fonction(y)-b.getpos().x();
+        FVector<float,2> p(-1,diff);
+        b.addition(p);
+        if(contact(b))
+        {
+            return false;
+        }
+        else
+            return true;
+    }
+
+}
+
+int fonction(int x)
+{
+    return hauteur/2+ cos(2*x*M_PI/largeur)*hauteur/8 + sin(3*x*M_PI/largeur)*hauteur/20;
+}
+
+int terrain::f(int i)
+{
+    return image[i];
+}
