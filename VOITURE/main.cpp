334cf999ba0645352421c9e6425a5e42f627410f
SamuelDiai <38350776+SamuelDiai@users.noreply.github.com>
2018-05-25 20:45:56 +0200

    Update main.cpp

--- VOITURE/main.cpp
+++ VOITURE/main.cpp
@@ -8,7 +8,6 @@ int main()
     srand((unsigned int)time(0));
     terrain t;
     Balle b;
-    Balle vieille_balle = b;
     openWindow(largeur,hauteur);
     t.afficher(b.getpos().x());
 
@@ -17,12 +16,9 @@ int main()
         if ((T%pasdaff)==0)
         {
         noRefreshBegin();
-        vieille_balle.effacer_balle();
-
+        b.effacer_balle();
         t.afficher(b.getpos().x());
-        vieille_balle=b;
         b.afficher_balle();
-        vieille_balle.afficher_balle();
         noRefreshEnd();
         milliSleep(50);
         }
