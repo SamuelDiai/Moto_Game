9b2c1f9c40178bb19a86070be816b97b02ec6da2
SamuelDiai <38350776+SamuelDiai@users.noreply.github.com>
2018-05-25 20:34:27 +0200

    Update voiture.h

--- VOITURE/voiture.h
+++ VOITURE/voiture.h
@@ -1,4 +1,27 @@
 #ifndef VOITURE_H
 #define VOITURE_H
+ #include <Imagine/Graphics.h>
+using namespace Imagine;
 
+const float dt=1;
+const int hauteur=400;
+const int largeur=400;
+
+class Balle
+{
+FVector<float,2> pos;
+FVector<float,2> vitesse;
+
+public :
+int r = 4;
+Balle();
+void effacer_balle();
+void update_air();
+void afficher_balle();
+FVector<float,2> getpos();
+void addition(FVector<float,2> vec);
+int getvitx();
+int getvity();
+
+};
 #endif // VOITURE_H
